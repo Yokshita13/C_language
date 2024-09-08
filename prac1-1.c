56bd857c6920df3dcd019b0868e59b12d45b4f83
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-09-08 15:16:08 +0530

    Create prac1-1.c

---
+++ prac1-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int length,width;
+    printf("Enter Length: ");
+    scanf("%d" ,&length);
+    printf("Enter Width: ");
+    scanf("%d" ,&width);
+    printf("The area of rectangle is: %d", length*width);
+
+    return 0;
+}
