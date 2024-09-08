ed4b6291af3b604101b0d5cb53a83ae3d089c38d
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-09-08 15:23:50 +0530

    Create prac1-3.c

---
+++ prac1-3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int r = 8;
+    int h = 4;
+    float volume = 3.14 * r * r * h;
+    printf("The volume of cylinder with radius %d and height %d is %f", r,h,volume);
+}
