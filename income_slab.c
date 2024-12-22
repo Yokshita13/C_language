ede44f959879d3a062ea6b9ad8fc19ffb7e454a7
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-22 23:11:06 +0530

    Create income_slab.c

---
+++ income_slab.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int main() {
+    int income;
+    int tax = 0;
+    
+    printf("Enter your income amount: ");
+    scanf("%d",&income);
+    
+    if(income<250000){
+        tax = 0;
+    }
+    else if(income>250000 && income<500000){
+        tax = 0.05 * (income - 250000);
+    }
+    else if(income>50000 && income<100000){
+        tax = 0.05 * (50000 - 250000) + 0.2 * (income - 50000);
+    }
+    else{
+        tax = 0.05 * (50000 - 250000) + 0.2 * (100000 - 50000) + 0.3 * (income - 100000);
+    }
+    
+    printf("The total tax you need to pay is %d",tax);
+    
+    return 0;
+}
