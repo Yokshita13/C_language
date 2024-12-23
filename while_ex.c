2a6fee732ae9581ab3530fce6c4166af46f19535
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-23 14:55:05 +0530

    Create while_ex.c

---
+++ while_ex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while(i<100){
+        printf("Dil todne wale ke liye Maya ke paas sirf ek tohfa hai—\nkhoon ke aansoon aur zindagi ka afsos.\n");
+        i++;
+    }
+    return 0;
+}
