b7db990116472f65f7a95e85569c130b0fdd0b26
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-23 14:56:40 +0530

    Update while_ex.c

--- while_ex.c
+++ while_ex.c
@@ -3,7 +3,7 @@
 int main() {
     int i = 0;
     while(i<100){
-        printf("Dil todne wale ke liye Maya ke paas sirf ek tohfa hai—\nkhoon ke aansoon aur zindagi ka afsos.\n");
+        printf("Dil todne wale ke liye Maya ke paas sirf ek tohfa hai—\n khoon ke aansoon aur zindagi ka afsos.\n");
         i++;
     }
     return 0;
