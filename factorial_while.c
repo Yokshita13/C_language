6821950581318b780d818f7a74909f7a35ae9233
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-25 15:12:42 +0530

    Update factorial_while.c

--- factorial_while.c
+++ factorial_while.c
@@ -3,8 +3,10 @@
 int main() {
     int product = 1;
     int n = 4;
-    for(int i=1; i<=n; i++){
+    int i = 1;
+   while(i<=n){
         product*=i;
+        i++;
     }
     printf("The factorial is %d",product);
     return 0;
