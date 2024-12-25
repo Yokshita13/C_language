0ec64764dda4207acbec2c672fc0704df27fef93
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-25 14:04:44 +0530

    Update sum_of_natural_no.c

--- sum_of_natural_no.c
+++ sum_of_natural_no.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    //USING WHILE LOOP
     int i = 1;
     int sum = 0;
     while(i<=10){
@@ -8,5 +9,23 @@ int main() {
         i++;
     }
     printf("The sum of natural numbers are %d \n",sum);
+
+    //USING DO-WHILE LOOP
+    int i = 1;
+    int sum = 0;
+    do{
+        sum += i;
+        i++;
+    }while(i<=10);
+    printf("The sum of natural numbers are %d \n",sum);
+
+    //USING FOR LOOP
+    int sum = 0;
+    for(int i=1; i<=10; i++){
+        sum+=i;
+    }
+    printf("The sum of natural numbers are %d \n",sum);
+    
     return 0;
 }
+
