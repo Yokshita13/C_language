221fcd50d3b9d2aec358a4a6c961af0c6a5070c2
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-25 13:42:12 +0530

    Create sumofnaturalno.c

---
+++ sumofnaturalno.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    int sum = 0;
+    while(i<=10){
+        sum += i;
+        i++;
+    }
+    printf("The sum of natural numbers are %d \n",sum);
+    return 0;
+}
