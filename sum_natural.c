43a04199e70c041ab4d494b9a8192003d5095bf8
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-29 23:14:32 +0530

    Create sum_natural.c

---
+++ sum_natural.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int sum_natural(int);
+int sum_natural(int n){
+    if(n==1){
+        return 1;
+    }
+    return sum_natural(n-1) + n;
+}
+int main() {
+    printf("The sum of first n natural numbers is %d\n",sum_natural(5));
+    return 0;
+}
