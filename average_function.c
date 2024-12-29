1daf452ea3dfa39e5a0e8d5df976e14ef066f33a
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-12-29 22:08:33 +0530

    Create average_function.c

---
+++ average_function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float avg(int a, int b, int c);
+float avg(int a, int b, int c){
+    return (a+b+c)/3;
+}
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 4;
+    printf("The average of a, b and c is %f",avg(a,b,c));
+    return 0;
+}
