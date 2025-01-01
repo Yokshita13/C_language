3d4ee81f8ac8549424bee3d0723b07085caa1a7a
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-01-01 16:46:09 +0530

    Update value_by_reference.c

--- value_by_reference.c
+++ value_by_reference.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
+void ten_times(int*);
+void ten_times(int* a){
+    *a = *a * 10;
+}
+
 int main() {
-    int i = 45;
-    int* j = &i;
-    int** k = &j;
-    int*** l = &k;
-    int**** m = &l;
-    int***** n = &m;
-    int****** o = &n;
-    int******* p = &o;
-    int******** q = &p;
-    int********* r = &q;
-    printf("The  address of i is %p\n",&i);
-    printf("The  address of i is %p\n",j);
-    printf("The  address of i is %p\n",k);
-    printf("The  address of i is %p\n",l);
-    printf("The  address of i is %p\n",m);
-    printf("The  address of i is %p\n",n);
-    printf("The  address of i is %p\n",o);
-    printf("The  address of i is %p\n",p);
-    printf("The  address of i is %p\n",q);
-    printf("The  address of i is %p\n",r);
+    int x = 45;
+    printf("The value of a is %d\n",x);
+    ten_times(&x);
+    printf("The value of a is %d\n",x);
   return 0;
 }
