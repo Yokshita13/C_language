dbb91a0c496d7ea33edfa653b4d5f9568e252460
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-01-01 16:09:17 +0530

    Update address_of_var.c

--- address_of_var.c
+++ address_of_var.c
@@ -4,6 +4,6 @@ int main() {
     int i = 45;
     int* j = &i;
     printf("The  address of i is %p\n",&i);
-    printf("The  address of i is %p\n",j);
+    printf("The  value of i is %d\n",*j);
   return 0;
 }
