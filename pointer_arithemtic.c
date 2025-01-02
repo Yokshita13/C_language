69b5688aed7038a8b03062d0b95e72a065de3513
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-01-02 22:59:28 +0530

    Update pointer_arithemtic.c

--- pointer_arithemtic.c
+++ pointer_arithemtic.c
@@ -3,7 +3,7 @@
 int main() {
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int* ptr = arr;
-    printf("The value at address %u is %d\n", ptr+3, *(ptr+3));
+    printf("The value at address %u is %d\n", ptr+2, *(ptr+2));
     
     return 0;
 }
