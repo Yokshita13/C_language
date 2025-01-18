dcfc03d0c669c8ca05b60fc87db2c45c30281a62
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-01-18 22:58:35 +0530

    Create checking.c

---
+++ checking.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char c = 'Y';
+    int contains = 0;
+    char str[] = "Yokshita";
+    
+    for(int i=0; i<strlen(str); i++)
+    {
+        if(str[i] == c)
+        {
+            contains = 1;
+            break;
+        }
+    }
+    
+    if(contains)
+    {
+        printf("Yes it contains \n");
+    }
+    else
+    {
+        printf("Does not contains \n");
+    }
+    
+    return 0;
+}
